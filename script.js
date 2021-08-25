b1cd3959f6d9f32594f964864e6b238d582a0483
Esteban Palacio <estipalacio@gmail.com>
2021-08-25 12:01:34 -0400

    added js func for modal

---
+++ script.js
@@ -0,0 +1,18 @@
+const modal = document.getElementById('modal')
+const modalShow = document.getElementById('show-modal')
+const modalClose = document.getElementById('close-modal')
+const bookmark = document.getElementById('bookmark-form')
+const websiteNameEl = document.getElementById('website-name')
+const websiteUrlEl = document.getElementById('website-url')
+const bookmarksContainer = document.getElementById('bookmarks-container')
+
+// Show modal, focus on Input
+const showModal = () => {
+    modal.classList.add('show-modal')
+    websiteNameEl.focus()
+}
+
+// Modal Event Listener
+modalShow.addEventListener('click', showModal)
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false))

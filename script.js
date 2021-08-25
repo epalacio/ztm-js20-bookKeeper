8cba3c1d6ca72a78935053f693810ae184ddbcb3
Esteban Palacio <estipalacio@gmail.com>
2021-08-25 14:39:58 -0400

    Added regEx form validation:

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const modal = document.getElementById('modal')
 const modalShow = document.getElementById('show-modal')
 const modalClose = document.getElementById('close-modal')
-const bookmark = document.getElementById('bookmark-form')
+const bookmarkForm = document.getElementById('bookmark-form')
 const websiteNameEl = document.getElementById('website-name')
 const websiteUrlEl = document.getElementById('website-url')
 const bookmarksContainer = document.getElementById('bookmarks-container')
@@ -16,3 +16,36 @@ const showModal = () => {
 modalShow.addEventListener('click', showModal)
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false))
+
+// Validate form
+const validate = (nameValue, urlValue) => {
+    const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+    const regex = new RegExp(expression)
+    if (!nameValue || !urlValue) {
+        alert('Please submit values for both fields')
+        return false
+    }
+    if (!urlValue.match(regex)) {
+        alert('Pleae provide a valid web address')
+        return false
+    }
+    // Valid
+    return true
+}
+
+// Handle Data from Form
+const storeBookmark = (e) => {
+    e.preventDefault()
+    const nameValue = websiteNameEl.value
+    let urlValue = websiteUrlEl.value
+    if (!urlValue.includes('http://', 'https://')) {
+        urlValue = `https://${urlValue}`
+    }
+    console.log(nameValue, urlValue);
+    if (!validate(nameValue, urlValue)) {
+       return false 
+    }
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark)
